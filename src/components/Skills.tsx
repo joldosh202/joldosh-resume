aa1bc6d40e783a10e85b522485729170ddb19019
JoldoshAjimudunov <joldoshbek8856@gmail.com>
2023-08-01 20:53:52 +0600

    added-aos

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,101 +4,107 @@ const Skills = () => {
    return (
       <div>
          <div className='bg-gray-700 h-[730px] sm:h-[600px] w-full flex flex-col items-center text-white '>
-          <div className='text-[30px] font-bold mt-10 mb-10 font-montserrat'>
-            Skills
-          </div>
-          <div className='flex w- w-[360px] sm:w-[900px] justify-between  flex-col sm:flex-row sm:ml-0'>
-            <div className='w-[360px]'>
-              <div className='font-bold text-[20px] text-center mb-3 font-montserrat'>
-                Hard-Skills
-              </div >
-              <div className='flex flex-col justify-between h-[270px]'>
-                <div className='flex items-center justify-between'>
-                  <li>React </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '70%'}}></div>
+              <div className='text-[30px] font-bold mt-10 mb-10 font-montserrat'
+              data-aos='zoom-in'
+              data-aos-duration="3000"
+              >
+                Skills
+              </div>
+            <div className='flex w- w-[360px] sm:w-[900px] justify-between  flex-col sm:flex-row sm:ml-0' 
+            data-aos='zoom-in'
+            data-aos-duration="3000"
+            >
+              <div className='w-[360px]'>
+                <div className='font-bold text-[20px] text-center mb-3 font-montserrat'>
+                  Hard-Skills
+                </div >
+                <div className='flex flex-col justify-between h-[270px]'>
+                  <div className='flex items-center justify-between'>
+                    <li>React </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '70%'}}></div>
+                    </div>
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <li>JavaScript </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '75%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>JavaScript </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '75%'}}></div>
+                    </div>
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <li>Redux-toolkit </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>Redux-toolkit </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
+                    </div>
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <li>Node.js</li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '45%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>Node.js</li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '45%'}}></div>
+                    </div>
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <li>Express.js</li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '45%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>Express.js</li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '45%'}}></div>
+                    </div>
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <li>Tailwind</li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>Tailwind</li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
+                    </div>
                   </div>
-                </div>
 
-                <div className='flex items-center justify-between'>
-                  <li>CSS </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '85%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>CSS </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '85%'}}></div>
+                    </div>
+                  </div>
+                  <div className='flex items-center justify-between'>
+                    <li>HTML </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '95%'}}></div>
+                    </div>
                   </div>
                 </div>
-                <div className='flex items-center justify-between'>
-                  <li>HTML </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '95%'}}></div>
-                  </div>
                 </div>
-              </div>
-              </div>
-            <div className='mt-9 sm:mt-0'>
-              <div className='font-bold text-[20px] text-center mb-3 font-montserrat'>
+              <div className='mt-9 sm:mt-0'>
+                <div className='font-bold text-[20px] text-center mb-3 font-montserrat'>
 
-              Soft-Skills
-              </div>
-              <div className='flex flex-col justify-between h-[140px] w-[360px]'>
-                <div className='flex items-center justify-between'>
-                  <li>Collaboration </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
-                  </div>
+                Soft-Skills
                 </div>
-                <div className='flex items-center justify-between'>
-                  <li>Communication </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
+                <div className='flex flex-col justify-between h-[140px] w-[360px]'>
+                  <div className='flex items-center justify-between'>
+                    <li>Collaboration </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
+                    </div>
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <li>Critical Thinking</li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '75%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>Communication </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '65%'}}></div>
+                    </div>
                   </div>
-                </div>
-                <div className='flex items-center justify-between'>
-                  <li>Problem Solving </li>
-                  <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                    <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '85%'}}></div>
+                  <div className='flex items-center justify-between'>
+                    <li>Critical Thinking</li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '75%'}}></div>
+                    </div>
                   </div>
+                  <div className='flex items-center justify-between'>
+                    <li>Problem Solving </li>
+                    <div className="w-[200px] bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                      <div className="bg-blue-600 h-2.5 rounded-full" style={{'width': '85%'}}></div>
+                    </div>
+                  </div>
+                  
                 </div>
-                
-              </div>
-              </div>
-          </div>
-      </div>
+                </div>
+            </div>
+        </div>
       </div>
    );
 };
--- src/components/WorkExp.tsx
+++ src/components/WorkExp.tsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import {useRef} from "react"
-import Navbar from './Navbar';
+import React, {useEffect} from 'react';
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 const WorkExp = () => {
-   // interface ChildComponentProps {
-   //    bottomEl: React.MutableRefObject<HTMLDivElement | null>;
-   //  }
-   // const bottomEl = useRef<null | HTMLDivElement>(null);
-   
+   useEffect(() => {
+      Aos.init()
+   },[])
    return (
       <div>
          <div className='bg-gray-700 h-2'></div>
-         <div className='bg-gray-800 h-[600px] w-full flex flex-col items-center text-white '>
-        <div className='w-[360px] sm:w-[800px] mt-10 sm:mt-20'>
-          <div className='text-center text-[30px] font-bold mb-10 font-montserrat'>work exprerience</div>
-        
-          <div className='text-[20px]  font-bold mb-5 font-montserrat'>
-          I was participating in many hachathons and builded some projects with other talanted developers
-          </div>
-         <div className=' text-[20px] font-bold mb-5 font-montserrat'>
-          here's some of them:
-          </div>
-          <div className='flex flex-col justify-between h-[200px]'>
-            
-            <div className=' text-[20px] font-bold mb-5 font-montserrat'>
-                Smart Manage website for managing personal finances. And Our team got an award of best team
+            <div className='bg-gray-800 h-[600px] w-full flex flex-col items-center text-white '>
+               <div className='w-[360px] sm:w-[800px] mt-10 sm:mt-20' data-aos='fade-right'
+                  data-aos-duration="3000">
+                  <div className='text-center text-[30px] font-bold mb-10 font-montserrat'
+                     
+                     >work exprerience</div>
+         
+                     <div className='text-[20px]  font-bold mb-5 font-montserrat'>
+                     I was participating in many hachathons and builded some projects with other talanted developers
+                     </div>
+                        <div className=' text-[20px] font-bold mb-5 font-montserrat'>
+                        here's some of them:
+                        </div>
+                  <div className='flex flex-col justify-between h-[200px]'>
+               
+                  <div className=' text-[20px] font-bold mb-5 font-montserrat'>
+                  Smart Manage website for managing personal finances. And Our team got an award of best team
+                   </div>
+                     <div className=' text-[20px] font-bold mb-5 font-montserrat'>
+                           live link:  <a href='https://smartmanage-d365a.web.app/' className='text-blue-600' target='blank'>click</a>
+                     </div>
+                  <div className=' text-[20px] font-bold mb-5 font-montserrat'>
+                     "Jobsearch.kg" is a service that helps to find a job and recruit personnel in Kyrgyzstan!
+                  </div>
+               </div>
+               </div>
             </div>
-            <div className=' text-[20px] font-bold mb-5 font-montserrat'>
-                  live link:  <a href='https://smartmanage-d365a.web.app/' className='text-blue-600' target='blank'>click</a>
-            </div>
-              <div className=' text-[20px] font-bold mb-5 font-montserrat'>
-                "Jobsearch.kg" is a service that helps to find a job and recruit personnel in Kyrgyzstan!
-             </div>
-          </div>
-        </div>
-      </div>
-      <div className='bg-gray-500 h-2'></div>
+         <div className='bg-gray-500 h-2'></div>
 
       </div>
    );
